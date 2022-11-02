85033fbe35c9b84f701a2cb7aa10db9b71edd301
Gage Lodba <JerimiahOfficial@gmail.com>
2022-11-02 17:20:15 -0400

    Update nfs.sh

--- nfs lab/nfs.sh
+++ nfs lab/nfs.sh
@@ -44,51 +44,51 @@ exportfs -r
 EOF
 
 # Create the users margaret and katherine on w01
-echo "adminpass" | su root -c "useradd -u 2000 margaret"
-echo "adminpass" | su root -c "useradd -u 2001 katherine"
-echo "adminpass" | su root -c "echo "123" | passwd --stdin margaret"
-echo "adminpass" | su root -c "echo "123" | passwd --stdin katherine"
+echo "adminpass" | su -c "useradd -u 2000 margaret" root
+echo "adminpass" | su -c "useradd -u 2001 katherine" root
+echo "adminpass" | su -c "echo "123" | passwd --stdin margaret" root
+echo "adminpass" | su -c "echo "123" | passwd --stdin katherine" root
 
 # Create research group and add margaret and katherine to it on w01
-echo "adminpass" | su root -c "groupadd research"
-echo "adminpass" | su root -c "usermod -a -G research margaret"
-echo "adminpass" | su root -c "usermod -a -G research katherine"
+echo "adminpass" | su -c "groupadd research" root
+echo "adminpass" | su -c "usermod -a -G research margaret" root
+echo "adminpass" | su -c "usermod -a -G research katherine" root
 
 # Create the shared directories
-echo "adminpass" | su root -c "mkdir -p /nfs_shares/scratch"
-echo "adminpass" | su root -c "mkdir -p /nfs_shares/research"
-echo "adminpass" | su root -c "mkdir -p /nfs_shares/pub"
+echo "adminpass" | su -c "mkdir -p /nfs_shares/scratch" root
+echo "adminpass" | su -c "mkdir -p /nfs_shares/research" root
+echo "adminpass" | su -c "mkdir -p /nfs_shares/pub" root
 
 # Create fstab entries
-echo "adminpass" | su root -c "echo \"s01:/nfs_shares/scratch /nfs_shares/scratch nfs defaults 0 0\" >> /etc/fstab"
-echo "adminpass" | su root -c "echo \"s01:/nfs_shares/research /nfs_shares/research nfs defaults 0 0\" >> /etc/fstab"
-echo "adminpass" | su root -c "echo \"s01:/nfs_shares/pub /nfs_shares/pub nfs defaults 0 0\" >> /etc/fstab"
+echo "adminpass" | su -c "echo \"s01:/nfs_shares/scratch /nfs_shares/scratch nfs defaults 0 0\" >> /etc/fstab" root
+echo "adminpass" | su -c "echo \"s01:/nfs_shares/research /nfs_shares/research nfs defaults 0 0\" >> /etc/fstab" root
+echo "adminpass" | su -c "echo \"s01:/nfs_shares/pub /nfs_shares/pub nfs defaults 0 0\" >> /etc/fstab" root
 
 # Mount the shared directories
-echo "adminpass" | su root -c "mount -t nfs s01:/nfs_shares/scratch /nfs_shares/scratch"
-echo "adminpass" | su root -c "mount -t nfs s01:/nfs_shares/research /nfs_shares/research"
-echo "adminpass" | su root -c "mount -t nfs s01:/nfs_shares/pub /nfs_shares/pub"
+echo "adminpass" | su -c "mount -t nfs s01:/nfs_shares/scratch /nfs_shares/scratch" root
+echo "adminpass" | su -c "mount -t nfs s01:/nfs_shares/research /nfs_shares/research" root
+echo "adminpass" | su -c "mount -t nfs s01:/nfs_shares/pub /nfs_shares/pub" root
 
 # Chmod the directories
-echo "adminpass" | su root -c "chmod 777 /nfs_shares/scratch"
-echo "adminpass" | su root -c "chmod 777 /nfs_shares/research"
-echo "adminpass" | su root -c "chmod 777 /nfs_shares/pub"
+echo "adminpass" | su -c "chmod 777 /nfs_shares/scratch" root
+echo "adminpass" | su -c "chmod 777 /nfs_shares/research" root
+echo "adminpass" | su -c "chmod 777 /nfs_shares/pub" root
 
 # try to create a file in /nfs_shares/scratch
-echo "adminpass" | su root -c "echo \"test\" > /nfs_shares/scratch/test.txt"
-echo "123" | su -c "echo \"test\" >> /nfs_shares/scratch/test.txt" margaret
-echo "123" | su -c "echo \"test\" >> /nfs_shares/scratch/test.txt" katherine
+echo "adminpass" | su -c "echo \"test\" > /nfs_shares/scratch/root" root
+echo "123" | su -c "echo \"test\" >> /nfs_shares/scratch/margaret" margaret
+echo "123" | su -c "echo \"test\" >> /nfs_shares/scratch/katherine" katherine
 
 # try to create a file in /nfs_shares/research
 echo "root is expected to fail"
-echo "adminpass" | su root -c "echo \"test\" > /nfs_shares/research/test.txt"
-echo "123" | su -c "echo \"test\" >> /nfs_shares/research/test.txt" margaret
-echo "123" | su -c "echo \"test\" >> /nfs_shares/research/test.txt" katherine
+echo "adminpass" | su -c "echo \"test\" > /nfs_shares/research/test.txt" root
+echo "123" | su -c "echo \"test\" >> /nfs_shares/research/margaret" margaret
+echo "123" | su -c "echo \"test\" >> /nfs_shares/research/katherine" katherine
 
 # try to create a file in /nfs_shares/pub
-echo "adminpass" | su root -c "echo \"test\" > /nfs_shares/pub/test.txt"
-echo "123" | su -c "echo \"test\" >> /nfs_shares/pub/test.txt" margaret
-echo "123" | su -c "echo \"test\" >> /nfs_shares/pub/test.txt" katherine
+echo "adminpass" | su -c "echo \"test\" > /nfs_shares/pub/root" root
+echo "123" | su -c "echo \"test\" >> /nfs_shares/pub/margaret" margaret
+echo "123" | su -c "echo \"test\" >> /nfs_shares/pub/katherine" katherine
 
 # excute the script on s01
 sshpass -p "adminpass" ssh root@s01 /bin/sh << EOF
