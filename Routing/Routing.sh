1aac9c9b71d00de594a8ea18c04b089963fd8c64
Gage Lodba <JerimiahOfficial@gmail.com>
2022-11-08 19:01:26 -0500

    Update Routing.sh

--- Routing/Routing.sh
+++ Routing/Routing.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+curl -O https://rpmfind.net/linux/dag/redhat/el7/en/x86_64/dag/RPMS/sshpass-1.05-1.el7.rf.x86_64.rpm
+
+echo "userpass" | sudo -S -k yum install sshpass-1.05-1.el7.rf.x86_64.rpm -y
+
+# end of automation script
+
 # Networks
 #
 # Blue
@@ -14,44 +20,40 @@
 # enps0s3: 10.2.1.30
 
 # hosts files must be updated as appropriate. With host names r01yellow and r01blue configured.
-echo "10.2.1.1 r01yellow" >>/etc/hosts
-echo "10.1.1.1 r01blue" >>/etc/hosts
+# echo "10.2.1.1 r01yellow" >>/etc/hosts
+# echo "10.1.1.1 r01blue" >>/etc/hosts
 
 # Add a route to the blue for w01
-echo "10.1.1.1 r01" >>/etc/hosts
+# echo "10.1.1.1 r01" >>/etc/hosts
 
 # Add a route to the yellow for s02
-echo "10.2.1.1 r01" >>/etc/hosts
+# echo "10.2.1.1 r01" >>/etc/hosts
 
 # enable ip forwarding
-sysctl -w net.ipv4.ip_forward=1
+# sysctl -w net.ipv4.ip_forward=1
 
 # disable the firewall
-systemctl disable firewalld
-systemctl stop firewalld
-
-# Add a default gateway to y01
-ip route add default via
+# systemctl disable firewalld
+# systemctl stop firewalld
 
 # install the iproute package
-yum install -y iproute
-
-# restart the interfaces
-nmcli connection up enp0s8
-nmcli connection up enp0s3
+# yum install -y iproute
 
 # set the hostname to r01
-hostnamectl set-hostname r01
+# hostnamectl set-hostname r01
 
 # set the hostname to s02
-hostnamectl set-hostname s02
+# hostnamectl set-hostname s02
 
 # reboot the system
-systemctl reboot
+# systemctl reboot
 
 # We have a new connection named 'Wired connection 1', let's rename it enp0s8 to be consistent
-nmcli connection modify 'Wired connection 1' connection.id enp0s8
-nmcli connection modify enp0s8 ipv4.addresses 10.2.1.20/24
-nmcli connection modify enp0s8 ipv4.method manual
-nmcli connection modify enp0s8 ipv6.method disable
-nmcli connection up enp0s8
+# nmcli connection modify 'Wired connection 1' connection.id enp0s8
+# nmcli connection modify enp0s8 ipv4.addresses 10.2.1.20/24
+# nmcli connection modify enp0s8 ipv4.method manual
+# nmcli connection modify enp0s8 ipv6.method disable
+# nmcli connection up enp0s8
+
+# Tell NetworkManager to send packets destined for the yellow network to s01's blue adapter.
+# nmcli connection modify enp0s8 ipv4.routes "10.2.1.1/24 10.1.1.0/24"
