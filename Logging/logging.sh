9ef684ec66e69c523a6b95d5360f83a96820a2fc
Gage Lodba <JerimiahOfficial@gmail.com>
2022-12-02 18:14:37 -0500

    Update logging.sh

--- Logging/logging.sh
+++ Logging/logging.sh
@@ -10,16 +10,14 @@ chmod +x sshpass-1.05-1.el7.rf.x86_64.rpm
 
 # install sshpass
 echo "Installing sshpass"
-sshpass -p "adminpass" ssh -o StrictHostKeyChecking=no root@w01 /bin/bash <<EOF
+sshpass -p "adminpass" ssh -o StrictHostKeyChecking=no root@w01 /bin/bash <<-EOF
+    # install sshpass from alice's downloads folder
+    yum install /home/alice/Downloads/sshpass-1.05-1.el7.rf.x86_64.rpm -y -q
 
-# install sshpass from alice's downloads folder
-yum install /home/alice/Downloads/sshpass-1.05-1.el7.rf.x86_64.rpm -y -q
-
-# make sure sshpass is installed before continuing
-while [ ! -f /usr/bin/sshpass ]; do
-sleep 1
-done
-
+    # make sure sshpass is installed before continuing
+    while [ ! -f /usr/bin/sshpass ]; do
+        sleep 1
+    done
 EOF
 
 # notify the user that sshpass is installed
@@ -31,80 +29,76 @@ sshpass -p "adminpass" scp -o StrictHostKeyChecking=no ./host_info_log.sh root@s
 
 # Running commands on s01 as root
 echo "Executing script on s01"
-sshpass -p "adminpass" ssh -o StrictHostKeyChecking=no root@s01 /bin/sh <<EOF
+sshpass -p "adminpass" ssh -o StrictHostKeyChecking=no root@s01 /bin/sh <<-EOF
+    # make sure that the script is executable
+    chmod +x /root/host_info_log.sh
 
-# make sure that the script is executable
-chmod +x /root/host_info_log.sh
+    # Part A:
+    logger -p cron.debug "FM1: This is fake msg from cron with pri=debug"
+    logger -p mail.err "FM2: This is fake msg from mail with pri=err"
+    logger -p local7.err "FM3: This is fake msg from local7 with pri=err"
 
-# Part A:
-logger -p cron.debug "FM1: This is fake msg from cron with pri=debug"
-logger -p mail.err "FM2: This is fake msg from mail with pri=err"
-logger -p local7.err "FM3: This is fake msg from local7 with pri=err"
+    # Part B:
+    cp /etc/rsyslog.conf /etc/rsyslog.conf.prev
 
-# Part B:
-cp /etc/rsyslog.conf /etc/rsyslog.conf.prev
+    echo "mail.* /var/log/mail_warn.log" >>/etc/rsyslog.conf
+    echo "*.err /var/log/msg_err.log" >>/etc/rsyslog.conf
 
-echo "mail.* /var/log/mail_warn.log" >>/etc/rsyslog.conf
-echo "*.err /var/log/msg_err.log" >>/etc/rsyslog.conf
+    systemctl restart rsyslog
 
-systemctl restart rsyslog
+    while [ "$(systemctl is-active rsyslog)" != "active" ]; do
+    sleep 1
+    done
+        
+    logger -p mail.err "FM4: mail.err"
+    logger -p mail.warning "FM5: mail.warn"
+    logger -p mail.debug "FM6: mail.debug"
+    logger -p cron.warning "FM7: cron.warn"
+    logger -p cron.err "FM8: cron.err"
 
-while [ "$(systemctl is-active rsyslog)" != "active" ]; do
-sleep 1
-done
-    
-logger -p mail.err "FM4: mail.err"
-logger -p mail.warning "FM5: mail.warn"
-logger -p mail.debug "FM6: mail.debug"
-logger -p cron.warning "FM7: cron.warn"
-logger -p cron.err "FM8: cron.err"
+    # Part C:
+    echo "module(load=\"imtcp\") # needs to be done just once" >>/etc/rsyslog.conf
+    echo "input(type=\"imtcp\" port=\"514\")" >>/etc/rsyslog.conf
 
-# Part C:
-echo "module(load=\"imtcp\") # needs to be done just once" >>/etc/rsyslog.conf
-echo "input(type=\"imtcp\" port=\"514\")" >>/etc/rsyslog.conf
+    firewall-cmd --permanent --add-port=514/tcp
+    firewall-cmd --reload
 
-firewall-cmd --permanent --add-port=514/tcp
-firewall-cmd --reload
+    while [ "$(firewall-cmd --state)" != "running" ]; do
+    sleep 1
+    done
 
-while [ "$(firewall-cmd --state)" != "running" ]; do
-sleep 1
-done
+    # Part D:
+    echo "local2.* /var/log/httpd_err" >>/var/log/httpd_err
 
-# Part D:
-echo "local2.* /var/log/httpd_err" >>/var/log/httpd_err
+    # Part E:
+    echo "local2.* ~" >>/etc/rsyslog.conf
+    echo "local2.* ~" >>/etc/rsyslog.conf
 
-# Part E:
-echo "local2.* ~" >>/etc/rsyslog.conf
-echo "local2.* ~" >>/etc/rsyslog.conf
-
-# run script
-/tmp/host_info_log.sh
-
+    # run script
+    /tmp/host_info_log.sh
 EOF
 
 # Running commands on w01 as root
 echo "Executing script on w01"
-sshpass -p "adminpass" ssh -o StrictHostKeyChecking=no root@w01 /bin/bash <<EOF
+sshpass -p "adminpass" ssh -o StrictHostKeyChecking=no root@w01 /bin/bash <<-EOF
+    # Part C:
+    echo "authpriv.* @@s01:514" >>/etc/rsyslog.conf
 
-# Part C:
-echo "authpriv.* @@s01:514" >>/etc/rsyslog.conf
+    # Part D:
+    yum install -y httpd -q
 
-# Part D:
-yum install -y httpd -q
+    echo "ErrorLog syslog:local2" >>/etc/httpd/conf/httpd.conf
 
-echo "ErrorLog syslog:local2" >>/etc/httpd/conf/httpd.conf
+    systemctl start httpd
 
-systemctl start httpd
+    while [ "$(systemctl is-active httpd)" != "active" ]; do
+    sleep 1
+    done
 
-while [ "$(systemctl is-active httpd)" != "active" ]; do
-sleep 1
-done
+    curl http://localhost
 
-curl http://localhost
+    # Part E:
+    echo "local2.* ~" >>/etc/rsyslog.conf
 
-# Part E:
-echo "local2.* ~" >>/etc/rsyslog.conf
-
-curl http://localhost
-
+    curl http://localhost
 EOF
