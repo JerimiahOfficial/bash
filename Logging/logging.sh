6d3fbb6bd40bccc9d582da457bb2acb9adb6f512
Gage Lodba <JerimiahOfficial@gmail.com>
2022-12-02 18:26:05 -0500

    Update logging.sh

--- Logging/logging.sh
+++ Logging/logging.sh
@@ -5,9 +5,6 @@ echo "Downloading sshpass"
 curl -O https://rpmfind.net/linux/dag/redhat/el7/en/x86_64/dag/RPMS/sshpass-1.05-1.el7.rf.x86_64.rpm
 curl -O https://raw.githubusercontent.com/JerimiahOfficial/bash/main/Logging/host_info_log.sh
 
-# chmod sshpass
-chmod +x sshpass-1.05-1.el7.rf.x86_64.rpm
-
 # install sshpass
 echo "Installing sshpass"
 echo "userpass" | sudo -S -k yum install sshpass-1.05-1.el7.rf.x86_64.rpm -y -q
